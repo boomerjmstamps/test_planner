b3889fc0b22270dbe43cfc97ddf4f65d781e9e53
Boomer <boomerjmstamps@gmail.com>
2015-03-13 20:38:52 -0300

    Inital commit of planner

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planner::Application.config.secret_token = '94a91c6a918e793ab9fb7b49e98763d9123d1f74f9e12c914ab5fff98a2551a0aa6c2bc954b18701ba719ac8d0c04edfe4bae988c5da6f99de05a8d7bd19ef57'
---
+++ db/migrate/20150313230806_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.time :start_time
+      t.time :end_time
+      t.text :description
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
