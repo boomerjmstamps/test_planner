96195b3f2bab53c4be5d95e87ca264eb8e65f3b1
Boomer <boomerjmstamps@gmail.com>
2015-03-13 22:58:33 -0300

    Added root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Planner::Application.routes.draw do
   resources :events
-
-
+  get 'events/index'
+  root :to => 'events#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,7 +51,6 @@ Planner::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
